c07134fb6e3498f83c9b45478a10f14f1f86bb80
Ben Vanderbosch <59888891+bennyyyv@users.noreply.github.com>
2022-02-01 11:38:47 -0700

    Updated Week 3

--- Week 3/Asteroid/main.lua
+++ Week 3/Asteroid/main.lua
@@ -5,3 +5,78 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+-- Initialize variables
+local lives = 3
+local score = 0
+local died = false
+ 
+local asteroidsTable = {}
+ 
+local ship
+local gameLoopTimer
+local livesText
+local scoreText
+
+local physics = require( "physics" )
+physics.start()
+physics.setGravity( 0, 0 )
+
+-- Seed the random number generator
+math.randomseed( os.time() )
+
+-- Load the background
+local backGroup = display.newGroup()
+
+local background = display.newImageRect("background.png", 800, 1400 )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+
+backGroup:insert(background)
+
+local sheetOptions =
+{
+    frames =
+    {
+        {   -- 1) asteroid 1
+            x = 0,
+            y = 0,
+            width = 102,
+            height = 85
+        },
+        {   -- 2) asteroid 2
+            x = 0,
+            y = 85,
+            width = 90,
+            height = 83
+        },
+        {   -- 3) asteroid 3
+            x = 0,
+            y = 168,
+            width = 100,
+            height = 97
+        },
+        {   -- 4) ship
+            x = 0,
+            y = 265,
+            width = 98,
+            height = 79
+        },
+        {   -- 5) laser
+            x = 98,
+            y = 265,
+            width = 14,
+            height = 40
+        },
+    },
+}
+
+local objectSheet = graphics.newImageSheet( "gameObjects.png", sheetOptions )
+
+local mainGroup = display.newGroup()
+ship = display.newImageRect( objectSheet, 4, 98, 79 )
+ship.x = display.contentCenterX
+ship.y = display.contentHeight - 100
+physics.addBody( ship, { radius=30, isSensor=true } )
+ship.myName = "ship"
+mainGroup:insert(ship)
