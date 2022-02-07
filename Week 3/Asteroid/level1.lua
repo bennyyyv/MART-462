e24437e195b98a4095294e6dac2b1290d4d64a18
Ben Vanderbosch <59888891+bennyyyv@users.noreply.github.com>
2022-02-06 21:48:07 -0700

    Finished Week 3

---
+++ Week 3/Asteroid/level1.lua
@@ -0,0 +1,363 @@
+-----------------------------------------------------------------------------------------
+--
+-- level1.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+-- Initialize variables
+local lives = 3
+local score = 0
+local died = false
+ 
+local asteroidsTable = {} -- this is an array (called tables in Lua)
+ local currentDelay = 1500
+local ship
+local gameLoopTimer
+local livesText
+local scoreText
+local timeToChange = 0 
+
+function scene:create( event )
+local sceneGroup = self.view
+
+end
+
+local physics = require( "physics" )
+physics.start()
+physics.setGravity( 0, 0 )
+
+
+
+-- Seed the random number generator
+math.randomseed( os.time() )
+
+-- Load the background
+local backGroup = display.newGroup()
+
+local background = display.newImageRect("background.png", 800, 1400 )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+--special group for background items 
+backGroup:insert(background)
+
+local sheetOptions =
+{
+    --coordinates on sprite sheet
+    frames =
+    {
+        {   -- 1) asteroid 1
+            x = 0,
+            y = 0,
+            width = 102,
+            height = 85
+        },
+        {   -- 2) asteroid 2
+            x = 0,
+            y = 85,
+            width = 90,
+            height = 83
+        },
+        {   -- 3) asteroid 3
+            x = 0,
+            y = 168,
+            width = 100,
+            height = 97
+        },
+        {   -- 4) ship
+            x = 0,
+            y = 265,
+            width = 98,
+            height = 79
+        },
+        {   -- 5) laser
+            x = 98,
+            y = 265,
+            width = 14,
+            height = 40
+        },
+    },
+}
+
+local objectSheet = graphics.newImageSheet( "gameObjects.png", sheetOptions )
+
+--main group for interactive items
+local mainGroup = display.newGroup()
+-- Ship variable = normal display code (objectSheet, #on objectSheet, size)
+ship = display.newImageRect( objectSheet, 4, 98, 79 )
+-- center object
+ship.x = display.contentCenterX
+ship.y = display.contentHeight - 100
+physics.addBody( ship, { radius=30, isSensor=true } )
+ship.myName = "ship"
+mainGroup:insert(ship)
+
+-- Display lives and score
+local uiGroup = display.newGroup()
+livesText = display.newText( "Lives: " .. lives, 200, 80, native.systemFont, 36 )
+scoreText = display.newText( "Score: " .. score, 400, 80, native.systemFont, 36 )
+uiGroup:insert(livesText)
+uiGroup:insert(scoreText)
+
+-- Hide the status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+local function updateText()
+    livesText.text = "Lives: " .. lives
+    scoreText.text = "Score: " .. score
+end
+
+local function createAsteroid()
+   local mainGroup = display.newGroup()
+   --reference the object sheet and the # on sheet
+    local newAsteroid = display.newImageRect( mainGroup, objectSheet, 1, 102, 85 )
+    --table is an array
+    table.insert( asteroidsTable, newAsteroid )
+    physics.addBody( newAsteroid, "dynamic", { radius=40, bounce=0.8 } )
+    newAsteroid.myName = "asteroid"
+ 
+    local whereFrom = math.random( 3 )
+    mainGroup:insert(newAsteroid)
+
+     if ( whereFrom == 1 ) then
+        -- From the left
+        newAsteroid.x = -60
+        newAsteroid.y = math.random( 500 )
+        newAsteroid:setLinearVelocity( math.random( 40,120 ), math.random( 20,60 ) )
+    elseif ( whereFrom == 2 ) then
+        -- From the top
+        newAsteroid.x = math.random( display.contentWidth )
+        newAsteroid.y = -60
+        newAsteroid:setLinearVelocity( math.random( -40,40 ), math.random( 40,120 ) )
+    elseif ( whereFrom == 3 ) then
+        -- From the right
+        newAsteroid.x = display.contentWidth + 60
+        newAsteroid.y = math.random( 500 )
+        newAsteroid:setLinearVelocity( math.random( -120,-40 ), math.random( 20,60 ) )
+          elseif ( whereFrom == 3 ) then
+        -- From the right
+        newAsteroid.x = display.contentWidth + 60
+        newAsteroid.y = math.random( 500 )
+        newAsteroid:setLinearVelocity( math.random( -120,-40 ), math.random( 20,60 ) )
+    end
+ 
+    newAsteroid:applyTorque( math.random( -6,6 ) )
+
+     mainGroup:insert(newAsteroid)
+
+end
+
+createAsteroid()
+
+
+local function fireLaser()
+    local mainGroup = display.newGroup()
+    local newLaser = display.newImageRect(objectSheet, 5, 14, 40 )
+    physics.addBody( newLaser, "dynamic", { isSensor=true } )
+    newLaser.isBullet = true
+    newLaser.myName = "laser"
+     newLaser.x = ship.x
+    newLaser.y = ship.y
+    newLaser:toBack()
+     transition.to( newLaser, { y=-40, time=500,
+        onComplete = function() display.remove( newLaser ) end
+    } )
+     mainGroup:insert(newLaser)
+end
+
+ship:addEventListener( "tap", fireLaser )
+
+local function dragShip( event )
+ local ship = event.target
+ local phase = event.phase
+
+  if ( "began" == phase ) then
+        -- Set touch focus on the ship
+        display.currentStage:setFocus( ship )
+        -- Store initial offset position
+        ship.touchOffsetX = event.x - ship.x
+        ship.touchOffsetY = event.y - ship.y
+    elseif ( "moved" == phase ) then
+        -- Move the ship to the new touch position
+        ship.x = event.x - ship.touchOffsetX
+        ship.y = event.y - ship.touchOffsetY
+         elseif ( "ended" == phase or "cancelled" == phase ) then
+        -- Release touch focus on the ship
+        display.currentStage:setFocus( nil )
+
+        -- Store initial offset position
+ship.touchOffsetX = event.x - ship.x
+ship.touchOffsetY = event.y - ship.y
+
+--ship.x = event.x - ship.touchOffsetX
+--ship.y = event.y - ship.touchOffsetY
+
+
+    end
+     return true  -- Prevents touch propagation to underlying objects
+end
+
+ship:addEventListener( "touch", dragShip )
+
+local function gameLoop()
+
+   if(timeToChange >= 5) then 
+    currentDelay = currentDelay - 10
+    print(currentDelay)
+    timeToChange = 0
+  end
+createAsteroid()
+
+    for i = #asteroidsTable, 1, -1 do
+    local thisAsteroid = asteroidsTable[i]
+ 
+        if ( thisAsteroid.x < -100 or
+             thisAsteroid.x > display.contentWidth + 100 or
+             thisAsteroid.y < -100 or
+             thisAsteroid.y > display.contentHeight + 100 )
+        then
+            display.remove( thisAsteroid )
+            table.remove( asteroidsTable, i )
+        end
+     end
+     timer.cancel(gameLoopTimer)
+end
+
+local function startGameLoop ()
+gameLoopTimer = timer.performWithDelay( currentDelay, gameLoop, 0 )
+
+end 
+
+gameLoopTimer = timer.performWithDelay( currentDelay, gameLoop, 0 )
+otherTimer = timer.performWithDelay( 2000, startGameLoop, 0 )
+timeToChange = timeToChange + 1
+
+local function restoreShip()
+ 
+    ship.isBodyActive = false
+    ship.x = display.contentCenterX
+    ship.y = display.contentHeight - 100
+ 
+    -- Fade in the ship
+    transition.to( ship, { alpha=1, time=4000,
+        onComplete = function()
+            ship.isBodyActive = true
+            died = false
+        end
+    } )
+end
+
+local function endGame()
+   composer.gotoScene( "menu", { time=800, effect="crossFade" } )
+end
+
+local function onCollision( event )
+ 
+  if ( event.phase == "began" ) then
+ 
+        local obj1 = event.object1
+        local obj2 = event.object2
+ 
+       if ( ( obj1.myName == "laser" and obj2.myName == "asteroid" ) or
+             ( obj1.myName == "asteroid" and obj2.myName == "laser" ) )
+        then
+            -- Remove both the laser and asteroid
+            display.remove( obj1 )
+            display.remove( obj2 )
+ 
+            for i = #asteroidsTable, 1, -1 do
+                if ( asteroidsTable[i] == obj1 or asteroidsTable[i] == obj2 ) then
+                    table.remove( asteroidsTable, i )
+                    break
+                end
+            end
+ 
+             -- Increase score
+            score = score + 100
+            scoreText.text = "Score: " .. score
+ 
+           elseif ( ( obj1.myName == "ship" and obj2.myName == "asteroid" ) or
+                 ( obj1.myName == "asteroid" and obj2.myName == "ship" ) )
+        then
+            if ( died == false ) then
+                died = true
+ 
+                -- Update lives
+                lives = lives - 1
+                livesText.text = "Lives: " .. lives
+ 
+                if ( lives == 0 ) then
+                    display.remove( ship )
+                    timer.performWithDelay( 2000, endGame )
+                else
+                    ship.alpha = 0
+                    timer.performWithDelay( 1000, restoreShip )
+                end
+            end
+        end
+    end
+end
+
+Runtime:addEventListener( "collision", onCollision )
+
+function scene:show( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if phase == "will" then
+		-- Called when the scene is still off screen and is about to move on screen
+	elseif phase == "did" then
+		-- Called when the scene is now on screen
+		-- 
+		-- INSERT code here to make the scene come alive
+		-- e.g. start timers, begin animation, play audio, etc.
+		physics.start()
+	end
+end
+
+function scene:hide( event )
+	local sceneGroup = self.view
+	
+	local phase = event.phase
+	
+	if event.phase == "will" then
+		-- Called when the scene is on screen and is about to move off screen
+		     
+		-- INSERT code here to pause the scene
+		-- e.g. stop timers, stop animation, unload sounds, etc.)
+		physics.stop()
+	elseif phase == "did" then
+		-- Called when the scene is now off screen
+         Runtime:removeEventListener( "collision", onCollision )
+        physics.pause()
+	end	
+	
+end
+
+function scene:destroy( event )
+
+	-- Called prior to the removal of scene's "view" (sceneGroup)
+	-- 
+	-- INSERT code here to cleanup the scene
+	-- e.g. remove display objects, remove touch listeners, save state, etc.
+	local sceneGroup = self.view
+	
+	package.loaded[physics] = nil
+	physics = nil
+end
+
+---------------------------------------------------------------------------------
+
+-- Listener setup
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
